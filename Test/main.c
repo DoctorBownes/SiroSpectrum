e8cf7c708ece2aa7dd7176cdcf1a0a8c2f7b606f
SimonR <sw.roeloffs@hotmail.com>
2023-09-08 13:10:20 +0200

    Animations somewhat fixed

--- Test/main.c
+++ Test/main.c
@@ -21,6 +21,44 @@ unsigned char stefanvas[8 * 16] = {
 	0,0,1,1,0,1,1,0,
 };
 
+unsigned char stefanvas1[8 * 16] = { 
+	0,0,0,1,1,1,0,0,
+0,0,1,1,1,1,1,0,
+0,1,1,1,1,1,1,0,
+0,1,1,1,0,1,0,0,
+0,1,1,1,1,1,1,0,
+0,0,1,1,1,1,0,0,
+0,0,0,1,1,0,0,0,
+0,0,1,0,0,1,0,0,
+0,1,1,1,1,1,1,0,
+0,1,0,1,1,1,1,0,
+0,0,1,1,1,1,0,1,
+0,1,0,0,0,0,0,0,
+0,0,1,1,1,1,0,0,
+0,1,1,1,0,1,1,0,
+1,1,0,0,0,1,1,1,
+0,1,1,0,0,0,1,0,
+};
+
+unsigned char stefanvas2[8 * 16] = {
+0,0,0,1,1,1,0,0,
+0,0,1,1,1,1,1,0,
+0,1,1,1,1,1,1,0,
+0,1,1,1,0,1,0,0,
+0,1,1,1,1,1,1,0,
+0,0,1,1,1,1,0,0,
+0,0,0,1,1,0,0,0,
+0,0,1,0,0,1,0,0,
+0,1,1,1,1,1,1,0,
+0,1,0,1,1,1,1,0,
+0,1,0,1,1,1,0,0,
+0,0,1,0,0,0,0,0,
+0,0,0,1,1,1,0,0,
+0,0,1,1,0,1,0,0,
+0,0,1,0,1,0,0,0,
+0,0,0,1,0,1,0,0, 
+};
+
 unsigned char wall[8 * 8] = {
 	0,1,0,0,0,0,0,0,
 	0,1,0,0,0,0,0,0,
@@ -41,58 +79,81 @@ unsigned char cat[] ={
 };
 Sprite Wall = {wall, 8, 8};
 Sprite Stefan = {stefanvas, 8, 16};
+Sprite Stefan1 = {stefanvas1, 8, 16};
+Sprite Stefan2 = {stefanvas2, 8, 16};
 Sprite downey = { cat, 8, 6 };
+Sprite stef_walk[3];
 
 unsigned char plr_x = 0;
 unsigned char plr_y = 0;
 unsigned char dir = 0b0001000;
+unsigned char frame = 0;
+unsigned char timer = 0;
+
+void constring() {
+	plr_x = 0;
+	plr_y = 0;
+	stef_walk[0] = Stefan;
+	stef_walk[1] = Stefan1;
+	stef_walk[2] = Stefan2;
+	//for (int i = 0; i < 256; i += 8) {
+	//	SetSprite(Wall, i, 0, 1);
+	//	SetBGColour(i / 8, 0, 2);
+	//}
+	//for (int i = 8; i < 192; i += 8) {
+	//	SetSprite(Wall, 0, i, 1);
+	//	SetBGColour(0, i / 8, 2);
+	//}
+	//for (int i = 8; i < 256; i += 8) {
+	//	SetSprite(Wall, i, 184, 1);
+	//	SetBGColour(i / 8, 23, 2);
+	//}
+	//for (int i = 8; i < 192; i += 8) {
+	//	SetSprite(Wall, 248, i, 1);
+	//	SetBGColour(31, i / 8, 2);
+	//}
+	//for (int i = 0; i < 24; i++) {
+	//	for (int j = 0; j < 32; j++) {
+	//		SetBGColour(j, i, 2);
+	//	}
+	//}
+}
 
 void loopdiloop() {
 	RemoveSprite(Stefan, plr_x, plr_y);
 	if (GetKey(Right)) {
-		plr_x +=2;
-		dir = 0b00001000;
+		//if (plr_x < 240) {
+			plr_x += 2;
+			dir = 0b00001000;
+		//}
 	}
 	 if (GetKey(Left)) {
-		plr_x -=2;
-		dir = 0b00011000;
+		// if (plr_x > 8) {
+			 plr_x -= 2;
+			 dir = 0b00011000;
+		// }
 	}
 	 if (GetKey(Up)) {
-		plr_y -=2;
+		// if (plr_y > 8) {
+			 plr_y -= 2;
+		// }
 	}
 	 if (GetKey(Down)) {
-		plr_y += 2;
+		// if (plr_y < 168) {
+			 plr_y += 2;
+		// }
 	}
-	SetSprite(Stefan, plr_x, plr_y, 8);
-	SetSprite(downey, 100, 100, 3);
-}
-void constring() {
-	printf("constructing\n");
-	plr_x = 8;
-	plr_y = 8;
+	SetSprite(stef_walk[frame], plr_x, plr_y, dir);
+	SetSprite(downey, 128, 100, 7);
 
-	for (int i = 0; i < 256; i += 8) {
-		SetSprite(Wall, i, 0, 1);
-		SetBGColour(i / 8, 0, 2);
-	}
-	for (int i = 8; i < 192; i += 8) {
-		SetSprite(Wall, 0, i, 1);
-		SetBGColour(0, i / 8, 2);
-	}
-	for (int i = 8; i < 256; i += 8) {
-		SetSprite(Wall, i, 184, 1);
-		SetBGColour(i / 8, 23, 2);
-	}
-	for (int i = 8; i < 192; i += 8) {
-		SetSprite(Wall, 248, i, 1);
-		SetBGColour(31, i / 8, 2);
-	}
-	for (int i = 0; i < 24; i++) {
-		for (int j = 0; j < 32; j++) {
-			SetBGColour(j, i, 2);
+	if (timer / 8) {
+		frame++;
+		if (frame >= 3) {
+			frame = 0;
 		}
+		timer = 0;
 	}
-	SetPixel(101, 105, 2);
+	timer++;
 }
 
 int main(void) {
